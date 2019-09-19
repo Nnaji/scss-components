b19975f2b50f1bf803adb97d7605cdbc462ca33d
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-09-19 18:45:34 +0200

    added src folder

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**************************************************************
+	@AUTHOR: Kingsley Nnaji <kingsley.nnaji@gmail.com>
+	@LICENSE: MIT
+	Creation Date : 19.09.2019
+***************************************************************/
+const css = require('./sass/main.scss');
+console.log('Hello there');
